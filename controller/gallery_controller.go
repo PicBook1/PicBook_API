aeb23eb8d9493e914827fc49e11f5174c4db2caf
avavirus3 <49475731+avavirus3@users.noreply.github.com>
2020-01-12 17:30:37 +0300

    Update gallery_controller.go

--- controller/gallery_controller.go
+++ controller/gallery_controller.go
@@ -1,1 +1,15 @@
 package controller
+
+import (
+	"encoding/json"
+	. "github.com/picbook1/dao"
+	"net/http"
+	"github.com/gorilla/mux"
+	"gopkg.in/mgo.v2/bson"
+	. "github.com/picbook1/models"
+
+)
+
+//var config = Config{}
+var gallery_dao = GallerysDAO{}
+
