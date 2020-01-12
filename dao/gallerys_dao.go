e83f58f3fd73b82d79d3f664aaffd33ae847e2fd
fasil251 <51713628+fasil251@users.noreply.github.com>
2020-01-12 03:02:20 -1200

    Update gallerys_dao.go

--- dao/gallerys_dao.go
+++ dao/gallerys_dao.go
@@ -7,3 +7,11 @@ import (
 	mgo "gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
+
+
+// Find list of gallerys
+func (m *GallerysDAO) FindAll() ([]Gallery, error) {
+	var gallerys []Gallery
+	err := gallerydb.C(GLy_COLLECTION).Find(bson.M{}).All(&gallerys)
+	return gallerys, err
+}
