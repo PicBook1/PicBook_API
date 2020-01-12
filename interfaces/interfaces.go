e37aea25bb9d57349eecf8ee7854822c5257c22d
fasil251 <51713628+fasil251@users.noreply.github.com>
2020-01-12 02:31:07 -1200

    Update interfaces.go

--- interfaces/interfaces.go
+++ interfaces/interfaces.go
@@ -2,5 +2,13 @@ package interfaces
 
 import "github.com/picbook1/models"
 
+type UserInterface interface {
+	Users() ([]models.User, []error)
+	User(id uint) (*models.User, []error)
+	UpdateUser(comment *models.User) (*models.User, []error)
+	DeleteUser(id uint) (*models.User, []error)
+	Close()
+}
+
 
 
