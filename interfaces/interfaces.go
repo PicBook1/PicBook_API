0ab348e65cc901871206f55cb94966e74325be4a
avavirus3 <49475731+avavirus3@users.noreply.github.com>
2020-01-12 17:29:55 +0300

    Update interfaces.go

--- interfaces/interfaces.go
+++ interfaces/interfaces.go
@@ -4,17 +4,3 @@ import "github.com/picbook1/models"
 
 
 
-import (
-	"encoding/json"
-	. "github.com/picbook1/dao"
-	"net/http"
-	"github.com/gorilla/mux"
-	"gopkg.in/mgo.v2/bson"
-	. "github.com/picbook1/models"
-
-)
-
-//var config = Config{}
-var gallery_dao = GallerysDAO{}
-
-
