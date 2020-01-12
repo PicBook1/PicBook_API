fcd0c410eb7e7b59f2e34fcd55e792d617ac1168
gerizangeru <50832367+gerizangeru@users.noreply.github.com>
2020-01-12 17:33:43 +0300

    Update gallery_controller.go

--- controller/gallery_controller.go
+++ controller/gallery_controller.go
@@ -13,6 +13,28 @@ import (
 //var config = Config{}
 var gallery_dao = GallerysDAO{}
 
+// GET a gallery by its ID
+func AllGallerys(w http.ResponseWriter, r *http.Request) {
+	gallerys, err := gallery_dao.FindAll()
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJson(w, http.StatusOK, gallerys)
+}
+
+// GET a gallery by its ID
+func FindGallery(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	gallery, err := gallery_dao.FindById(params["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid Gallery ID")
+		return
+	}
+	respondWithJson(w, http.StatusOK, gallery)
+}
+
+
 // DELETE an existing gallery
 func DeleteGallery(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
