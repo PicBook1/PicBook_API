3404a4ed0de90803d68439ff0952151d94fd8c74
Henokked <50827183+Henokked@users.noreply.github.com>
2020-01-12 03:13:27 -1200

    Update models.go

--- models/models.go
+++ models/models.go
@@ -7,3 +7,13 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
+
+type Gallery struct {
+	ID          bson.ObjectId `bson:"_id" json:"id"`
+	UserName    string        `bson:"username" json:"username"`
+	Title       string        `bson:"title" json:"title"`                  
+	Discription string        `bson:"discription" json:"discription"`
+	Path        string        `bson:"path" json:"path"`
+	Comment     []Comment     `bson:"comment" json:"comment"`
+	Rating      int
+}
