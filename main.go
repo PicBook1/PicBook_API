6293331c77a38c18bcd9e6b7a914abd3b1b430b1
Henokked <50827183+Henokked@users.noreply.github.com>
2020-01-12 02:17:36 -1200

    Update main.go

--- main.go
+++ main.go
@@ -15,9 +15,12 @@ func main() {
 	r := mux.NewRouter()
 	
 	r.HandleFunc("/users", controller.AllUsers).Methods("GET")
+	r.HandleFunc("/users", controller.UpdateUser).Methods("PUT")
 	
 if err := http.ListenAndServe(":8008", r); err != nil {
 		log.Fatal(err)
 	}
 
 }
+
+
