c0eb7baa2f0ceb2850c5de6eca06ccdddc3380e1
fasil251 <51713628+fasil251@users.noreply.github.com>
2020-01-12 02:06:43 -1200

    Update user_controller.go

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -1,1 +1,14 @@
 package controller
+
+
+
+
+// GET a user by its ID
+func AllUsers(w http.ResponseWriter, r *http.Request) {
+	users, err := user_dao.FindAll()
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJson(w, http.StatusOK, users)
+}
